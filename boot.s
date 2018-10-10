6e2f9145a29e2c3651539859e5983231da0ba733
Bernd Veidinger <berndv@uplinkgroup.eu>
2018-10-10 18:22:56 +0200

    Added working kernel, linker and boot sector

---
+++ boot.s
@@ -0,0 +1,35 @@
+/* Declare constants for the multiboot header. */
+.set ALIGN,     1<<0
+.set MEMINFO,   1<<1
+.set FLAGS,     ALIGN | MEMINFO
+.set MAGIC,     0x1BADB002
+.set CHECKSUM,  -(MAGIC + FLAGS)
+
+/* Magic numbers, so the bootloader will identify the file as kernel file. */
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .bss
+.align 16
+stack_bottom:
+      .skip 16384 # 16 KiB
+      stack_top:
+
+      .section .text
+      .global _start
+      .type _start, @function
+      _start:
+
+      mov $stack_top, %esp
+
+      call kernel_main
+
+      cli
+1:    hlt
+      jmp 1b
+
+.size _start, . - _start
